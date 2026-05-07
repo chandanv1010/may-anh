a9617ce343806f46b2b10584e4da3931885277a3
chandanv1010 <tuannc.dev@gmail.com>
2026-05-07 16:15:34 +0700

    fix: resolve critical build failure due to unbalanced JSX tags in calendar.tsx

--- resources/js/pages/backend/booking/calendar.tsx
+++ resources/js/pages/backend/booking/calendar.tsx
@@ -253,7 +253,6 @@ export default function BookingCalendar({ machines, users, bookings, catalogues 
                         </div>
                     </div>
                         {calendarGrid}
-                    </div>
                 </Card>
             </div>
 
